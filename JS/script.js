be22275ddfc4bd8d67345e745328114dd4b7476f
Tahesha <taheshao@gmail.com>
2024-05-08 16:52:51 -0400

    Adding Cache elements

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+// Cache elements
+const ideaList = document.getElementById('idea-list');
+const feedbackForm = document.getElementById('feedback-form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
